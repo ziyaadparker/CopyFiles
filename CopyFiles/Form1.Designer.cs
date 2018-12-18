fda8e2be011a21f6b21d64a49ff8ad0641574513
ziyaadparker <ziyaadparker@yahoo.co.uk>
2018-12-18 17:16:55 +0200

    Added the code to copy files

---
+++ CopyFiles/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CopyFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(101, 105);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 197);
+            this.Controls.Add(this.btnCopy);
+            this.Name = "Form1";
+            this.Text = "CopyForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ CopyFiles/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CopyFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        public void Copy()
+        {
+            string fileName = "test.txt";
+            string fileName2 = "test2.txt";
+            string sourcePath = @"C:\Users\Ziyaad\Documents\Visual Studio 2012\Projects\CopyFiles\CopyFiles\bin\Debug\Test1";
+            string targetPath = @"C:\Users\Ziyaad\Documents\Visual Studio 2012\Projects\CopyFiles\CopyFiles\bin\Debug\Test2";
+
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            string targetFile = System.IO.Path.Combine(targetPath, fileName2);
+
+            if (!System.IO.Directory.Exists(targetPath))
+            {
+                System.IO.Directory.CreateDirectory(targetPath);
+            }
+
+            System.IO.File.Copy(sourceFile, targetFile);
+
+        }
+
+        void CopyDirectory(string sourceDir, string targetDir)
+        {
+            System.IO.Directory.CreateDirectory(targetDir);
+
+            foreach (var file in System.IO.Directory.GetFiles(sourceDir))
+                System.IO.File.Copy(file, System.IO.Path.Combine(targetDir, System.IO.Path.GetFileName(file)));
+
+            foreach (var directory in System.IO.Directory.GetDirectories(sourceDir))
+                System.IO.File.Copy(directory, System.IO.Path.Combine(targetDir, System.IO.Path.GetFileName(directory)));
+        }
+    }
+}
